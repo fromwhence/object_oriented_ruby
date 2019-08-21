8dd4a5d05fc03ba9ed1abe17c9bca30116153375
fromwhence <wenc.matt@gmail.com>
2019-08-21 12:32:38 -0500

    test commit

---
+++ vehicle_examples/inheritance_example.rb
@@ -0,0 +1,60 @@
+# • Create a new file in your vehicle_examples folder called inheritance_example.rb and open it in Sublime.
+# • Copy the Car and Bicycle class (attached) into your file
+# • Use inheritance to DRY up the classes. Note - a car is NOT in a type of bicycle, and a bicycle is NOT a type of car!
+# • To test it, create a bicycle and car instance from your newly modified classes (you can create them at the bottom of your file). First, have them accelerate. Then, make sure a bike can "Ring ring!" and a car "Beeeeeeep!"
+
+class Vehicle
+
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+end
+
+class Car < Vehicle
+  def initialize(input_options)
+    @fuel = input_options[:fuel]
+    @make = input_options[:make]
+    @model = input_options[:model]
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+
+end
+
+class Bike < Vehicle
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+class Bike
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+
+end
+
+car = Car.new
+bike = Bike.new
+
+
+
+
